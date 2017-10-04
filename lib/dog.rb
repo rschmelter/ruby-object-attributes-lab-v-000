0e5e7249950b861dac75120612f581e66e76ac19
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-04 00:50:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name
+    @name
+  end
+
+  def  name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job_type)
+    @job = job_type
+  end
+
+end
