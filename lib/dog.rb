3f106ed9dd77fa9c8541b1f30c4bfdb89c9d3d35
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-04 00:46:46 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,37 @@
+class Dog
+  def name
+    @name
+  end
+
+  name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  breed=(breed_type)
+    @breed = breed_type
+  end
+
+end
+
+class Person
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def job
+    @job
+  end
+
+  job=(job_type)
+  @job = job_type
+end
+
+end
